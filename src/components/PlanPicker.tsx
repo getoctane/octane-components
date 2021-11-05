3b6642ebeca1075e9f5bc06425e04235bd54506b
salemhilal <salemhilal@gmail.com>
2021-11-05 17:53:35 -0400

    Start styling the price plan components
    This adds a stylesheet, makes the story show three (identical for now) price plans, and adds a few classes to the PlanPicker components to start getting things styled

--- src/components/PlanPicker.tsx
+++ src/components/PlanPicker.tsx
@@ -1,19 +1,47 @@
 import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { formatCurrency } from 'utils/format';
 
-export interface Props {
+export interface PricePlansProps {
   customerToken: string;
 }
 
-/* eslint-disable @typescript-eslint/naming-convention, @typescript-eslint/camelcase */
-interface PricePlan {
+export interface PricePlanCardProps {
+  pricePlan: PricePlan;
+}
+
+export interface PricePlan {
   name: string;
   display_name: string;
   description: string;
   base_price: number;
   period: string;
+  coupon?: {
+    name: string;
+  };
+  discount?: {
+    discount_type: string;
+    amount: number;
+    start_date: string;
+    end_date: string;
+    coupon_id: number;
+  };
+  features?: {
+    name: string;
+    display_name: string;
+    description: string;
+  }[];
+  limits?: {
+    feature: {
+      name: string;
+      display_name: string;
+      description: string;
+    };
+    limit: number;
+  }[];
   metered_components: {
     meter_name: string;
+    limit: number;
     price_scheme: {
       display_name: string;
       name: string;
@@ -26,13 +54,38 @@ interface PricePlan {
       }[];
     };
   }[];
+  tags: { tag: string }[];
+  trial?: {
+    time_length: number;
+    time_unit_name: string;
+    credit: number;
+  };
 }
-/* eslint-enable @typescript-eslint/naming-convention, @typescript-eslint/camelcase */
 
 const TokenContext = React.createContext<string>('NO_TOKEN');
 
-function PricePlanCard({ pricePlan }: { pricePlan: PricePlan }): JSX.Element {
-  return <div>Plan name: {pricePlan.display_name} </div>;
+function PricePlanCard({ pricePlan }: PricePlanCardProps): JSX.Element {
+  const {
+    display_name: displayName,
+    base_price: basePrice,
+    period,
+  } = pricePlan;
+  const billingSchedule =
+    period === 'month' ? 'Billed monthly' : `Billed every ${period}`;
+  const formattedBasePrice = formatCurrency(basePrice);
+  const periodAbbrev = period === 'month' ? 'mo' : period;
+  return (
+    <div className='price-plan-card'>
+      <div className='plan-name'>{displayName} </div>
+      <div className='base-price'>
+        <span className='label starting-at'>Starting at </span>
+        <span className='cost'>{formattedBasePrice}</span>
+        <span className='period'>/{periodAbbrev}</span>
+      </div>
+      <div className='separator' />
+      <div className='label billing-sched'>{billingSchedule}</div>
+    </div>
+  );
 }
 
 function PricePlanManager(): JSX.Element {
@@ -51,15 +104,16 @@ function PricePlanManager(): JSX.Element {
 
   const token = useContext(TokenContext);
   return (
-    <div>
-      <div>Token: "{token}"</div>
+    <div className='octane-component price-plan-picker'>
       {pricePlans.map((plan) => (
         <PricePlanCard key={plan.name} pricePlan={plan} />
       ))}
     </div>
   );
 }
-export default function PricePlans({ customerToken }: Props): JSX.Element {
+export default function PricePlans({
+  customerToken,
+}: PricePlansProps): JSX.Element {
   return (
     <TokenContext.Provider value={customerToken}>
       <PricePlanManager />
