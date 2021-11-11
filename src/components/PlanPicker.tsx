e7f4674ff62960189ebe503af9f2416a6b8c9f0f
salemhilal <salemhilal@gmail.com>
2021-11-11 16:15:11 -0500

    Hit the API to determine the actively selected price plan.

--- src/components/PlanPicker.tsx
+++ src/components/PlanPicker.tsx
@@ -2,15 +2,19 @@ import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { components } from 'apiTypes';
 import { PricePlanCard } from 'components/PricePlanCard';
-import { fetchPricePlans } from 'utils/api';
+import { fetchPricePlans, fetchCustomerActiveSubscription } from 'utils/api';
 
 export type PricePlan = components['schemas']['PricePlan'];
 export type MeteredComponent = components['schemas']['MeteredComponent'];
 export interface PlanPickerProps {
   /**
-   * An API token with permissions for a specific customer
+   * An API token with permissions for a specific customer.
    */
   customerToken: string;
+  /**
+   * The name of the customer to update the subscription for.
+   */
+  customerName: string;
   /**
    * The name of the currently selected plan, if any.
    * This is the initial value that the PlanPicker's internal
@@ -19,13 +23,32 @@ export interface PlanPickerProps {
   initialSelected?: string;
 }
 
-const TokenContext = React.createContext<string>('NO_TOKEN');
+const TokenContext = React.createContext<{
+  token: string;
+  customerName: string;
+}>({
+  token: 'NO_TOKEN',
+  customerName: 'NO_CUSTOMER',
+});
+
+type LoadingState =
+  // Loading has started, but it's too soon to show a loading state
+  | 'preload'
+  // It's taken some time, show a loading state
+  | 'loading'
+  // The component has all the data it needs
+  | 'loaded';
 
 function PricePlanManager(): JSX.Element {
   const [pricePlans, setPricePlans] = useState<PricePlan[]>([]);
-  const token = useContext(TokenContext);
+  const [selected, setSelected] = useState<string | undefined>(undefined);
+  const [loading, setLoading] = useState<LoadingState>('preload');
+
+  const { token, customerName } = useContext(TokenContext);
+
   useEffect(() => {
-    const fetchData = async (): Promise<void> => {
+    // Get all price plans
+    const pricePlans = async (): Promise<void> => {
       const result = await fetchPricePlans({ token });
       if (!result.ok) {
         throw new Error('Something went wrong fetching price plans');
@@ -33,29 +56,53 @@ function PricePlanManager(): JSX.Element {
       const data = await result.json();
       setPricePlans(data);
     };
-    fetchData();
-  }, [setPricePlans, token]);
+    // Get the current subscription, if one exists
+    const activeSubscription = async (): Promise<void> => {
+      const result = await fetchCustomerActiveSubscription(
+        { token },
+        customerName
+      );
+      if (!result.ok) {
+        throw new Error('Something went wrong fetching price plans');
+      }
+      const data = await result.json();
+      if (data !== null) {
+        setSelected(data.price_plan?.name);
+      }
+    };
 
-  const [selected, setSelected] = useState<string | undefined>(undefined);
+    setTimeout(() => {
+      if (loading === 'preload') {
+        setLoading('loading');
+      }
+    });
+
+    Promise.all([activeSubscription(), pricePlans()]).then(() => {
+      setLoading('loaded');
+    });
+  }, [setLoading, setPricePlans, setSelected, token]);
 
   return (
     <div className='octane-component price-plan-picker'>
-      {pricePlans.map((plan) => (
-        <PricePlanCard
-          key={plan.name}
-          pricePlan={plan}
-          selected={plan.name === selected}
-          onSelect={setSelected}
-        />
-      ))}
+      {loading === 'loading' && <div className='loading'>Loading...</div>}
+      {loading === 'loaded' &&
+        pricePlans.map((plan) => (
+          <PricePlanCard
+            key={plan.name}
+            pricePlan={plan}
+            selected={plan.name === selected}
+            onSelect={setSelected}
+          />
+        ))}
     </div>
   );
 }
 export default function PlanPicker({
-  customerToken,
+  customerToken: token,
+  customerName,
 }: PlanPickerProps): JSX.Element {
   return (
-    <TokenContext.Provider value={customerToken}>
+    <TokenContext.Provider value={{ token, customerName }}>
       <PricePlanManager />
     </TokenContext.Provider>
   );
