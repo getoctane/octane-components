856f58a54a91f402788eabab06649f7318d71238
salemhilal <salemhilal@gmail.com>
2021-11-12 18:51:10 -0500

    Make a subscribeCustomer() function
    I called this an 'action' which may be a little overloaded. Since it doesn't really have any hooks that are dependencies, I wasn't sure it made sense to make _this_ a hook as well.
    Why not make the storage thing use hooks, you might ask? Because the calls to 'set' might not happen in the same application scope as the calls to 'get', and because it's probably a bad idea to make application state depend on globals that can change.
    
    So, with all that in mind, I made a function that consumes what's currently on the global state and does API stuff with it. It's not the slickest thing ever, but it seems good and I think it should work (hard to say without some more API stuff and the billing component in place)

---
+++ src/actions/subscribeCustomer.ts
@@ -0,0 +1,60 @@
+import { selectedPricePlan, existingSubscription } from 'utils/sharedState';
+import { createSubscription, updateSubscription } from 'utils/api';
+import { components } from 'apiTypes';
+type ActiveSubscription = components['schemas']['ActiveSubscription'];
+
+export default function subscribeCustomer(
+  customerName: string,
+  customerToken: string
+): Promise<ActiveSubscription> {
+  const plan = selectedPricePlan.get();
+  if (plan === null) {
+    return Promise.reject(
+      new Error(
+        'No price plan was selected by the UI.' +
+          ' Make sure the PlanPicker component was rendered and that a plan was selected.'
+      )
+    );
+  }
+
+  if (existingSubscription.get() === null) {
+    return Promise.reject(
+      new Error(
+        "The customer's existing subscription was not checked." +
+          ' Make sure the PlanPicker component was rendered and that a plan was selected.'
+      )
+    );
+  }
+
+  if (existingSubscription.get() === 'no_existing_plan') {
+    return createSubscription(
+      {
+        token: customerToken,
+        body: {
+          price_plan_name: plan.name,
+        },
+      },
+      customerName
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(`Something went wrong creating a subscription.`);
+      }
+      return response.json();
+    });
+  } else {
+    return updateSubscription(
+      {
+        token: customerToken,
+        body: {
+          price_plan_name: plan.name,
+        },
+      },
+      customerName
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(`Something went wrong updating a subscription.`);
+      }
+      return response.json();
+    });
+  }
+}
