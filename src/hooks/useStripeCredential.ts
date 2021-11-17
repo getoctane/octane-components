e7760396fc9c5765ce7980eb5c3a0a09b36b889e
Dima Ivanyuk <dima@getoctane.io>
2021-11-17 03:54:54 -0500

    [ecp] Rework external fetching (stripe), move to use PK and stripe account ID as appropriate

--- src/hooks/useStripeCredential.ts
+++ src/hooks/useStripeCredential.ts
@@ -4,54 +4,55 @@ import {
 } from 'api/stripe';
 import { API_BASE } from 'config';
 import useCustomerToken from 'hooks/useCustomerToken';
-import { useEffect } from 'markdown-to-jsx/node_modules/@types/react';
+import { useCallback, useEffect } from 'react';
 import { createStripeSetupIntent } from 'utils/api';
 
 const useStripeCredential = (): void => {
   const { customerName, token } = useCustomerToken();
 
+  const setupIntent = useCallback(async (): Promise<void> => {
+    const result = await createStripeSetupIntent(
+      {
+        token,
+        urlOverride: API_BASE,
+        body: {
+          customer_name: customerName,
+        },
+      },
+      customerName
+    );
+    if (!result.ok) {
+      throw new Error(`An error occurred: ${result.statusText}`);
+    }
+    const data = await result.json();
+    if (data.client_secret == null) {
+      throw new Error(
+        `There was a problem authenticating. Refresh the page and try again.`
+      );
+    }
+    // Store the secret in memory so that its never saved to react state
+    // and can be externally referenced.
+    const currentClientSecret = getSetupIntentClientSecret();
+    if (
+      currentClientSecret == null &&
+      data.client_secret != null &&
+      currentClientSecret !== data.client_secret
+    ) {
+      // Update the in-memory secret if it is meaningful and has changed.
+      setSetupIntentClientSecret(data.client_secret);
+    }
+  }, [customerName, token]);
+
   useEffect(() => {
     // Kickoff a flow to create a setup intent, starting with acquiring the client
     // secret from the serverside.
-    const setupIntent = async (): Promise<void> => {
-      const result = await createStripeSetupIntent(
-        {
-          token,
-          urlOverride: API_BASE,
-          body: {
-            customer_name: customerName,
-          },
-        },
-        customerName
-      );
-      if (!result.ok) {
-        throw new Error(`An error occurred: ${result.statusText}`);
-      }
-      const data = await result.json();
 
-      if (data.client_secret == null) {
-        throw new Error(
-          `There was a problem authenticating. Refresh the page and try again.`
-        );
-      }
-      // Store the secret in memory so that its never saved to react state
-      // and can be externally referenced.
-      const clientSecret = getSetupIntentClientSecret();
-      if (
-        clientSecret == null &&
-        data.client_secret != null &&
-        clientSecret !== data.client_secret
-      ) {
-        // Update the in-memory secret if it is meaningful and has changed.
-        setSetupIntentClientSecret(data.client_secret);
-      }
-    };
     try {
       setupIntent();
     } catch (err) {
       console.error(err);
     }
-  }, [customerName, token]);
+  }, [setupIntent]);
 };
 
 export default useStripeCredential;
