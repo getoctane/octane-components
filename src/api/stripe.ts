bf8b4b63d6954474ed1d1c6b559ab2f6f9dfc9c5
Dima Ivanyuk <dima@getoctane.io>
2021-11-17 03:54:54 -0500

    [ecp] Rework external fetching (stripe), move to use PK and stripe account ID as appropriate

--- src/api/stripe.ts
+++ src/api/stripe.ts
@@ -1,21 +1,29 @@
 import { loadStripe } from '@stripe/stripe-js';
 import assert from 'assert';
 
-const stripeKey = process.env.OCT_STRIPE_PUBLIC_KEY;
+const OCT_PLATFORM_API_KEY = process.env.OCT_PLATFORM_API_KEY;
+const stripeAccountId = process.env.OCT_STRIPE_ACCOUNT_ID;
 assert(
-  stripeKey != null,
-  'Expected non-null Stripe (secret) key. Double-check that you are setting `OCT_STRIPE_PUBLIC_KEY` in your env variables.'
+  stripeAccountId != null,
+  'Expected non-null Stripe account ID. Double-check that you are setting `OCT_STRIPE_ACCOUNT_ID` in your env variables.'
+);
+assert(
+  OCT_PLATFORM_API_KEY != null,
+  'Expected non-null Stripe (public) key. Double-check that you are setting `OCT_PLATFORM_API_KEY` in your env variables.'
 );
 
-export const STRIPE_PUBLIC_KEY = stripeKey;
+export const STRIPE_ACCOUNT_ID = stripeAccountId;
 
-export const StripeApiPromise = loadStripe(STRIPE_PUBLIC_KEY);
+export const StripeApiPromise = loadStripe(OCT_PLATFORM_API_KEY, {
+  stripeAccount: STRIPE_ACCOUNT_ID,
+});
 
 // Use a simple let binding to avoid any type of stateful storage for this sensitive secret that
 // should be forwarded directly to the Stripe Elements components.
 let clientSecret: string;
 
 export const getSetupIntentClientSecret = (): string => clientSecret;
+
 export const setSetupIntentClientSecret = (s: string): void => {
   clientSecret = s;
 };
