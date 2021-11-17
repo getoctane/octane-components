1d84d09f46191c2dd387fa205596b8427ff2bdb0
Ahmed Shalaby <akshalaby@gmail.com>
2021-11-17 10:43:06 -0500

    Update API endpoints and response types

--- src/utils/api.test.ts
+++ src/utils/api.test.ts
@@ -11,27 +11,27 @@ describe('utils/api', () => {
   describe('getPricePlansUrl', () => {
     it('accepts a base URL', () => {
       expect(getPricePlansUrl()).toEqual(
-        'https://api.cloud.getoctane.io/price_plans/'
+        'https://api.cloud.getoctane.io/ecp/price_plans/'
       );
       expect(getPricePlansUrl('https://example.com')).toEqual(
-        'https://example.com/price_plans/'
+        'https://example.com/ecp/price_plans/'
       );
     });
   });
 
   describe('getPricePlans', () => {
     it('calls the price plan URL with no params', async () => {
-      fetchMock.mock('https://api.cloud.getoctane.io/price_plans/', 200);
+      fetchMock.mock('https://api.cloud.getoctane.io/ecp/price_plans/', 200);
       await getPricePlans({ token: 'some token' });
       expect(fetchMock).toHaveBeenCalled();
       expect(fetchMock.lastUrl()).toEqual(
-        'https://api.cloud.getoctane.io/price_plans/'
+        'https://api.cloud.getoctane.io/ecp/price_plans/'
       );
     });
 
     it('calls the price plan URL with query params', async () => {
       fetchMock.mock(
-        'https://api.cloud.getoctane.io/price_plans/?names=hey%2Cyou&tags=live',
+        'https://api.cloud.getoctane.io/ecp/price_plans/?names=hey%2Cyou&tags=live',
         200
       );
       await getPricePlans({
@@ -43,7 +43,7 @@ describe('utils/api', () => {
       });
       expect(fetchMock).toHaveBeenCalled();
       expect(fetchMock.lastUrl()).toEqual(
-        'https://api.cloud.getoctane.io/price_plans/?names=hey%2Cyou&tags=live'
+        'https://api.cloud.getoctane.io/ecp/price_plans/?names=hey%2Cyou&tags=live'
       );
     });
   });
