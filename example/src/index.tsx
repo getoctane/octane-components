e8373f13dd01a8d43ab52be4cdb1ef671467d1d1
shilal <shilal@etsy.com>
2021-11-25 17:09:24 +0100

    This renders real live plans on my screen

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+fetch('/token')
+  .then((resp) => resp.json())
+  .then((data) => {
+    const { token } = data;
+    ReactDOM.render(<App token={token} />, document.getElementById('root'));
+  });
