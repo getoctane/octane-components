0521edcf402ed6052e2f7c38b4a3362cd077c877
shilal <shilal@etsy.com>
2021-11-25 11:45:45 +0100

    Hit Octane's token endpoint
    This endpoint successfully gets me tokens for a user provided through a .env file.

--- example/server/index.js
+++ example/server/index.js
@@ -1,12 +1,33 @@
 // Imports
 require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
 
 // Constants
 const app = express();
 const port = process.env.PORT ?? 3000;
 const octaneKey = process.env.OCTANE_API_KEY;
 const userId = process.env.EXAMPLE_USER_ID;
+const octaneHost =
+  process.env.OCTANE_API_HOST ?? 'https://api.cloud.getoctane.io';
+const axiosConfig = {
+  headers: {
+    Authorization: `Bearer ${octaneKey}`,
+  },
+};
+
+console.log(`
+╭――――――――――――――――――――――――――――――――――――╮
+│                                    │
+│ Starting the Octane Example Server │
+│                                    │
+╰――――――――――――――――――――――――――――――――――――╯
+
+Environment:
+   - port: ${port}                  
+   - user ID: ${userId}             
+   - Octane host: ${octaneHost}     
+`);
 
 // Make sure our required configurations are set.
 if (!octaneKey) {
@@ -26,9 +47,24 @@ app.get('/ping', (req, res) => {
 app.use('public', express.static('public'));
 
 // Get the token for the given user
-app.get('/token/:user', (req, res) => {
+app.get('/token', (req, res) => {
   // Get the token for the user, given the API key
-  res.send(JSON.stringify(req));
+  axios
+    .post(
+      `${octaneHost}/ecp/token`,
+      {
+        customer_name: userId,
+      },
+      axiosConfig
+    )
+    .then((apiRes) => {
+      res.json(apiRes.data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500);
+      res.send('Something went wrong fetching a token for the current user.');
+    });
 });
 
 app.listen(port, () => {
