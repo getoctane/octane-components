63e00b660f80ec7d14d455ce1e92bde579c15946
shilal <shilal@etsy.com>
2021-11-29 18:46:41 -0500

    First end-to-end pass at the example app
    I made a real live subscription with this, which is sick.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
-import { PlanPicker, PaymentSubmission } from 'octane-components';
+import {
+  PlanPicker,
+  PaymentSubmission,
+  subscribeCustomer,
+} from 'octane-components';
 import type { PricePlan } from 'octane-components';
 
 interface Props {
@@ -8,16 +12,37 @@ interface Props {
 
 const App = ({ token }: Props): JSX.Element => {
   const [hasSelected, setHasSelected] = useState<boolean>(false);
+  const [hasBilling, setHasBilling] = useState<boolean>(false);
 
   // When any plan has been selected, show the PaymentSubmission
   const onPlanSelect = (_: string, plan: PricePlan): void => {
     setHasSelected(plan != null);
   };
 
+  const onBillingSubmit = (): void => {
+    setHasBilling(true);
+  };
+  const onSubscribe = (): void => {
+    subscribeCustomer(token).then(() => {
+      // eslint-disable-next-line no-console
+      console.log('Customer has been subscribed');
+    });
+  };
+
   return (
     <div>
       <PlanPicker customerToken={token} onPlanSelect={onPlanSelect} />
-      {hasSelected && <PaymentSubmission customerToken={token} />}
+      {hasSelected && (
+        <PaymentSubmission customerToken={token} onSubmit={onBillingSubmit} />
+      )}
+      {hasBilling && (
+        <button onClick={onSubscribe}>Click here to subscribe</button>
+      )}
+      <div>
+        <h2>Internal state</h2>
+        <div>Has a plan selected: {hasSelected ? 'yes' : 'no'}</div>
+        <div>Has submitted billing data: {hasBilling ? 'yes' : 'no'}</div>
+      </div>
     </div>
   );
 };
