154fea8055c0ec4744ddf4a90d297acd6f3b41d0
shilal <shilal@etsy.com>
2021-11-30 18:28:55 -0500

    Update example app to not use state
    This would ideally use the payment_method_status endpoint once it exists

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,25 +1,35 @@
 import React, { useState, useCallback } from 'react';
-import {
-  PlanPicker,
-  PaymentSubmission,
-  subscribeCustomer,
-} from 'octane-components';
-import type { PricePlan } from 'octane-components';
+import { PlanPicker, PaymentSubmission, Actions } from 'octane-components';
+import type { SchemaTypes } from 'octane-components';
+
+type PricePlan = SchemaTypes['PricePlan'];
+
+const { subscribeCustomer } = Actions;
 
 interface Props {
   token: string;
 }
 
+/**
+ * This is temporary; until we publish octane-components on NPM, we can't access
+ * types as we expect.
+ */
+interface ActiveSubscription {
+  price_plan: {
+    display_name: string;
+  };
+}
+
 const App = ({ token }: Props): JSX.Element => {
-  const [hasSelected, setHasSelected] = useState<boolean>(false);
+  const [selectedPlan, setSelectedPlan] = useState<PricePlan | null>(null);
   const [hasBilling, setHasBilling] = useState<boolean>(false);
 
   // When any plan has been selected, show the PaymentSubmission
   const onPlanSelect = useCallback(
     (_: string, plan: PricePlan): void => {
-      setHasSelected(plan != null);
+      setSelectedPlan(plan);
     },
-    [setHasSelected]
+    [setSelectedPlan]
   );
 
   const onBillingSubmit = useCallback((): void => {
@@ -27,12 +37,16 @@ const App = ({ token }: Props): JSX.Element => {
   }, [setHasBilling]);
 
   const onSubscribe = useCallback((): void => {
-    subscribeCustomer(token).then((data) => {
-      alert(`Customer has been subscribed to ${data.price_plan.display_name}`);
-      // eslint-disable-next-line no-console
-      console.log('subscription data', data);
-    });
-  }, [token]);
+    subscribeCustomer(token, selectedPlan.name).then(
+      (data: ActiveSubscription) => {
+        alert(
+          `Customer has been subscribed to ${data.price_plan.display_name}`
+        );
+        // eslint-disable-next-line no-console
+        console.log('subscription data', data);
+      }
+    );
+  }, [token, selectedPlan]);
 
   const cardStyle = {
     base: {
@@ -60,10 +74,11 @@ const App = ({ token }: Props): JSX.Element => {
   return (
     <div id='example-root'>
       <PlanPicker customerToken={token} onPlanSelect={onPlanSelect} />
-      {hasSelected && (
+      {selectedPlan != null && (
         <PaymentSubmission
           customerToken={token}
           onSubmit={onBillingSubmit}
+          // onBillingSet
           options={stripeOptions}
         />
       )}
@@ -74,7 +89,10 @@ const App = ({ token }: Props): JSX.Element => {
       )}
       <div>
         <h2>Internal state</h2>
-        <div>Has a plan selected: {hasSelected ? 'yes' : 'no'}</div>
+        <div>
+          Has a plan selected:{' '}
+          {selectedPlan ? `yes ${selectedPlan.display_name}` : 'no'}
+        </div>
         <div>Has submitted billing data: {hasBilling ? 'yes' : 'no'}</div>
       </div>
     </div>
