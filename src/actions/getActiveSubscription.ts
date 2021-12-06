98e15f64a9697fa82ac696535fdabd6d825dabfe
shilal <salem@getoctane.io>
2021-12-06 11:15:06 -0500

    Update changelog, fix typo in getActiveSubscription

--- src/actions/getActiveSubscription.ts
+++ src/actions/getActiveSubscription.ts
@@ -7,7 +7,7 @@ type PricePlan = components['schemas']['PricePlan'];
  * Fetches the customer's active subscription.
  * Resolves to the price plan for that subscription, or `null` if there is none.
  */
-export function hasPaymentInfo(
+export function getActiveSubscription(
   customerToken: string
 ): Promise<PricePlan | null> {
   return getCustomerActiveSubscription({ token: customerToken })
