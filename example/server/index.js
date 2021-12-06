1e8e02b19970ebbf99fadb90feeda770c8fc7f8e
shilal <shilal@etsy.com>
2021-12-06 15:39:18 -0500

    Make the example server error messages clearer
    and also return JSON.

--- example/server/index.js
+++ example/server/index.js
@@ -58,9 +58,25 @@ app.get('/token', (req, res) => {
       res.json(apiRes.data);
     })
     .catch((error) => {
-      console.error(error);
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
       res.status(500);
-      res.send('Something went wrong fetching a token for the current user.');
+      res.json({
+        error: 'Something went wrong fetching a token for the current user.',
+      });
     });
 });
 
