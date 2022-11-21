226f08602ade7b649dc1f177ac8fd08b574e3865
Vlad Poltorin <vlad@getoctane.io>
2022-11-21 18:03:58 -0500

    feat: add hook and action for fetching customer invoices

---
+++ src/actions/getInvoices.ts
@@ -0,0 +1,31 @@
+import { getCustomerInvoices } from '../api/octane';
+import { components } from '../apiTypes';
+
+type Invoices = components['schemas']['Invoice'][];
+
+type Options = {
+  baseApiUrl?: string;
+};
+
+/**
+ * Fetches the customer's invoices.
+ * Resolves to the array of invoices, or `null` if there is none.
+ */
+export default async function getInvoices(
+  customerToken: string,
+  options: Options = {}
+): Promise<Invoices | null> {
+  if (!customerToken) {
+    throw new Error('Token must be provided.');
+  }
+
+  const response = await getCustomerInvoices({
+    token: customerToken,
+    urlOverride: options.baseApiUrl,
+  });
+  if (!response.ok) {
+    throw new Error("Something went wrong fetching the customer's invoices");
+  }
+  const data = await response.json();
+  return data;
+}
---
+++ src/hooks/useInvoices.tsx
@@ -0,0 +1,32 @@
+import { useCallback, useContext } from 'react';
+import { useAsync } from './useAsync';
+import getInvoices from '../actions/getInvoices';
+import type { UseAsyncReturnType } from './useAsync';
+import { TokenContext } from './useCustomerToken';
+import { components } from '../apiTypes';
+
+type Invoices = components['schemas']['Invoice'][];
+
+export type UseInvoicesReturnType = UseAsyncReturnType<Invoices>;
+
+/**
+ * A hook that fetches the customer's invoices.
+ */
+export const useInvoices = (args?: {
+  token?: string;
+  baseApiUrl?: string;
+}): UseInvoicesReturnType => {
+  const { token: tokenFromContext } = useContext(TokenContext);
+  const userToken = args?.token || tokenFromContext;
+  const baseApiUrl = args?.baseApiUrl;
+
+  if (!userToken) {
+    throw new Error('Token must be provided.');
+  }
+
+  const asyncFunc = useCallback(() => {
+    return getInvoices(userToken, { baseApiUrl });
+  }, [userToken, baseApiUrl]);
+
+  return useAsync(asyncFunc);
+};
