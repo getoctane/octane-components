b29da00914aa6a15ba52bb5d3b49211179fcf575
Ahmed Shalaby <ahmed@getoctane.io>
2023-06-05 10:30:52 -0400

    feat: add update() to useActiveSubscription hook

--- src/hooks/useActiveSubscription.tsx
+++ src/hooks/useActiveSubscription.tsx
@@ -1,34 +1,65 @@
-import { useCallback, useContext } from 'react';
+import { useCallback, useContext, useMemo } from 'react';
 import getActiveSubscription from '../actions/getActiveSubscription';
 import { useAsync } from './useAsync';
 import type { UseAsyncFetchReturnType } from './useAsync';
 import { TokenContext } from './useCustomerToken';
 import { components } from '../apiTypes';
+import subscribeCustomer from '../actions/subscribeCustomer';
+import type { SubscribeCustomerOptions } from '../actions/subscribeCustomer';
 
 type ActiveSubscription =
   components['schemas']['CustomerPortalActiveSubscription'];
+export type ActivePricePlan =
+  components['schemas']['CustomerPortalSubscription'];
+export type ActiveSubscriptionInputArgs =
+  components['schemas']['CustomerPortalActiveSubscriptionInputArgs'];
 
 export type UseActiveSubscriptionReturnType =
-  UseAsyncFetchReturnType<ActiveSubscription>;
+  UseAsyncFetchReturnType<ActiveSubscription> & {
+    update: (pricePlanInfo: ActiveSubscriptionInputArgs) => void;
+  };
 
 /**
  * A hook that fetches the customer's active subscription information.
  */
+
 export const useActiveSubscription = (args?: {
   token?: string;
   baseApiUrl?: string;
+  options?: SubscribeCustomerOptions;
 }): UseActiveSubscriptionReturnType => {
   const { token: tokenFromContext } = useContext(TokenContext);
   const userToken = args?.token || tokenFromContext;
   const baseApiUrl = args?.baseApiUrl;
+  const options = useMemo(() => args?.options || {}, [args?.options]);
 
   if (!userToken) {
     throw new Error('Token must be provided.');
   }
 
-  const asyncFunc = useCallback(() => {
+  const fetchFn = useCallback(() => {
     return getActiveSubscription(userToken, { baseApiUrl });
   }, [userToken, baseApiUrl]);
 
-  return useAsync(asyncFunc);
+  const updateFn = useCallback(
+    (pricePlanInfo: ActiveSubscriptionInputArgs) => {
+      if (!pricePlanInfo?.price_plan_uuid) {
+        throw new Error('Price plan uuid must be provided.');
+      }
+
+      return subscribeCustomer(userToken, pricePlanInfo, {
+        ...options,
+        baseApiUrl,
+      });
+    },
+    [userToken, options, baseApiUrl]
+  );
+
+  const hook = useAsync(fetchFn);
+  return {
+    ...hook,
+    update: (pricePlanInfo: ActiveSubscriptionInputArgs) => {
+      updateFn(pricePlanInfo).then(() => hook.refetch());
+    },
+  };
 };
