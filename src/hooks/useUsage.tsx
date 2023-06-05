d5b902c9e04f7525f79ad05315c937b29cc4ee91
Ahmed Shalaby <ahmed@getoctane.io>
2023-06-05 11:14:11 -0400

    feat: add useUsage hook that is not onDemand

---
+++ src/hooks/useUsage.tsx
@@ -0,0 +1,37 @@
+import { useCallback, useContext } from 'react';
+import { useAsync } from './useAsync';
+import { TokenContext } from './useCustomerToken';
+import { components } from '../apiTypes';
+import getCusomerUsage from '../actions/getUsage';
+
+type CustomerPortalMeterLabelFilter =
+  components['schemas']['CustomerPortalMeterLabelFilter'];
+
+/**
+ * A hook that fetches the customer's usage.
+ */
+export const useUsage = (args?: {
+  meterFilter?: CustomerPortalMeterLabelFilter;
+  token?: string;
+  baseApiUrl?: string;
+}) => {
+  const { token: tokenFromContext } = useContext(TokenContext);
+  const userToken = args?.token || tokenFromContext;
+  const baseApiUrl = args?.baseApiUrl;
+
+  if (!userToken) {
+    throw new Error('Token must be provided.');
+  }
+
+  const fetchFunc = useCallback(
+    (meterFilter?: CustomerPortalMeterLabelFilter) => {
+      if (meterFilter == null) {
+        return Promise.resolve(null);
+      }
+      return getCusomerUsage(userToken, meterFilter, { baseApiUrl });
+    },
+    [userToken, baseApiUrl]
+  );
+
+  return useAsync(fetchFunc, [args?.meterFilter]);
+};
