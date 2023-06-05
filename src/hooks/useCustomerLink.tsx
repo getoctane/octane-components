2b7be2525d486b06dd207e188f33bf6885c220a0
Vlad Poltorin <vlad@getoctane.io>
2023-06-05 13:19:38 -0400

    fix: update customerLink hook with token context

--- src/hooks/useCustomerLink.tsx
+++ src/hooks/useCustomerLink.tsx
@@ -1,8 +1,9 @@
-import { useCallback } from 'react';
+import { useCallback, useContext } from 'react';
 import { useAsync } from './useAsync';
 import type { UseAsyncFetchReturnType } from './useAsync';
 import getCustomerLink from '../actions/getCustomerLink';
 import { components } from 'apiTypes';
+import { TokenContext } from './useCustomerToken';
 
 type ContactLink = components['schemas']['CustomerPortalUrl'];
 export type UseContactLinkReturnType = UseAsyncFetchReturnType<ContactLink>;
@@ -10,19 +11,21 @@ export type UseContactLinkReturnType = UseAsyncFetchReturnType<ContactLink>;
 /**
  * A hook that fetches the link to a customer's portal.
  */
-export const useCustomerLink = (args: {
-  token: string;
+export const useCustomerLink = (args?: {
+  token?: string;
   baseApiUrl?: string;
 }): UseContactLinkReturnType => {
-  const { token, baseApiUrl } = args;
+  const { token: tokenFromContext } = useContext(TokenContext);
+  const userToken = args?.token || tokenFromContext;
+  const baseApiUrl = args?.baseApiUrl;
 
-  if (!token) {
+  if (!userToken) {
     throw new Error("Customer's token must be provided.");
   }
 
   const fetchFn = useCallback(() => {
-    return getCustomerLink(token, { baseApiUrl });
-  }, [token, baseApiUrl]);
+    return getCustomerLink(userToken, { baseApiUrl });
+  }, [userToken, baseApiUrl]);
 
   return useAsync({ fetchFn });
 };
